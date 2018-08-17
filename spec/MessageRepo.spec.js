695bca715cfd264e4c551718fee00f768dc818ab
Joel Söderström <joel.soderstrom@gmail.com>
2018-08-17 15:40:44 +0200

    Improvemet: Private (#11)
    * Hide messages with subject prefix _private
    
    * Bumps deps
    
    * Removes empty test

--- spec/MessageRepo.spec.js
+++ spec/MessageRepo.spec.js
@@ -2,17 +2,16 @@ const testUtils = require("fruster-test-utils");
 const MessageRepo = require("../lib/repos/MessageRepo");
 
 describe("MessageRepo", () => {
-
 	let messageRepo;
 
 	testUtils.startBeforeEach({
 		mongoUrl: "mongodb://localhost:27017/fruster-message-logger",
-		afterStart: (conn) => {
+		afterStart: conn => {
 			messageRepo = new MessageRepo(conn.db);
 		}
 	});
 
-	it("should save transaction and set response time", (done) => {
+	it("should save transaction and set response time", done => {
 		const reqSubject = "fake-request";
 		const req = {
 			reqId: "aa8a4fc1-cb3f-4329-8bf4-3a0f31fbd61f",
@@ -30,21 +29,21 @@ describe("MessageRepo", () => {
 			transactionId: req.transactionId
 		};
 
-		messageRepo.save(reqSubject, req)
-			.then(savedReq => {
-				expect(savedReq.id).toBeDefined();
-				expect(savedReq.created).toBeDefined();
-				expect(savedReq.subject).toBe(reqSubject);
-				expect(savedReq.reqId).toBe(req.reqId);
-				expect(savedReq.message).toBeDefined();
-				expect(savedReq.from.service).toBe("service");
-				expect(savedReq.from.instanceId).toBe("instanceId");
-			});
+		messageRepo.save(reqSubject, req).then(savedReq => {
+			expect(savedReq.id).toBeDefined();
+			expect(savedReq.created).toBeDefined();
+			expect(savedReq.subject).toBe(reqSubject);
+			expect(savedReq.reqId).toBe(req.reqId);
+			expect(savedReq.message).toBeDefined();
+			expect(savedReq.from.service).toBe("service");
+			expect(savedReq.from.instanceId).toBe("instanceId");
+		});
 
-		messageRepo.save(resSubject, res)
+		messageRepo
+			.save(resSubject, res)
 			.then(wait)
 			.then(savedRes => messageRepo.get(savedRes.id))
-			.then(savedResWithTime => {				
+			.then(savedResWithTime => {
 				expect(savedResWithTime.reqSubject).toBe(reqSubject);
 				expect(savedResWithTime.ms).toBeGreaterThan(0);
 				done();
